f0729402c959ebc72706a9cdeedb2bc8fe3e26fc
SteveDaSteve <40248221+SteveDaSteve@users.noreply.github.com>
2018-06-13 19:07:17 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,1 +1,22 @@
-minetest.register_node(
+minetest.register_node("testmod:covered_stone",{
+		descrption = "Stone Table"
+		tiles = {
+			"testmod_stone_table_top.png",
+			"default_stone.png",
+			"testmod_stone_table_side.png",
+			"testmod_stone_table_side.png",
+			"testmod_stone_table_side.png",
+			"testmod_stone_table_side.png",
+},
+		is_ground_content = false,
+		groups = {cracky = 2, stone = 1}
+		drops = "testmod:covered_stone"
+})
+
+		minetest.register_craft({
+    output = "testmod:covered_stone",
+    recipe = {
+        {"group:wool"},
+        {"default:stone"},
+			}
+    })
