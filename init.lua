c1ad37bb069670bb410f9681eb1bb3aa1cbcfda8
SteveDaSteve <40248221+SteveDaSteve@users.noreply.github.com>
2018-06-14 17:33:48 -0400

    Attempt to add burning tablecloths

--- init.lua
+++ init.lua
@@ -9,7 +9,7 @@ minetest.register_node("testmod:covered_stone",{
 			"testmod_stone_table_side.png",
 },
 		is_ground_content = false,
-		groups = {cracky = 2, stone = 1},
+		groups = {cracky = 2, stone = 1, flamable = 1},
 		drops = "testmod:covered_stone"
 })
 
@@ -17,16 +17,20 @@ minetest.register_craft({
     output = "testmod:covered_stone",
     recipe = {
         {"group:wool"},
-        {"default:stone"},
+        {"group:stone"},
 			}
 })
 
 minetest.register_abm({
+		label = "Tablecloth Burning"
+		neighbors = "fire:basic_flame"
 		nodenames = {"testmod:covered_stone"},
-		neighbors = {"fire:basic_flame"},
+		neighbors = {"fire:basic_flame", "fire:permanent_flame"},
 		interval = 5,
 		chance = 5,
-		action = function(pos, node, active_object_count, active_object_count_wider)
-		minetest.set_node({x = pos.x, y = pos.y, z = pos.z}, {name = "default:stone"})
-	end
+		action = function(pos, node, active_object_count, active_object_count_wider) 
+			local fire_on_top = minetest.get_node{(x = pos.x, y = pos.y + 1, z = pos.z)}[1]
+			if fire_on_top == fire:basic_flame or fire_on_top == fire:permanent_flame then
+			minetest.swap_node(pos, default:stone)
+			end
 })
