d471c431f43eabc347a9c9da3b6e1899cb6cb686
SteveDaSteve <40248221+SteveDaSteve@users.noreply.github.com>
2018-06-18 21:17:56 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -20,3 +20,27 @@ minetest.register_craft({
         {"group:stone"},
 			}
 })
+
+minetest.register_node("home_blocks:light_fixture",{
+		description = "Light Bulb",
+		drawtype = "plantlike",
+		tiles = {
+			"home_blocks_stone_table_top.png",
+			"default_stone.png",
+			"home_blocks_stone_table_side.png",
+			"home_blocks_stone_table_side.png",
+			"home_blocks_stone_table_side.png",
+			"home_blocks_stone_table_side.png",
+},
+		is_ground_content = false,
+		groups = {cracky = 2, stone = 1, flamable = 1},
+		drops = "home_blocks:covered_stone"
+})
+
+minetest.register_craft({
+    output = "home_blocks:covered_stone",
+    recipe = {
+        {"group:wool"},
+        {"group:stone"},
+			}
+})
