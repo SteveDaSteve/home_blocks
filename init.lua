721f709e79e9729baf16ecd0916e72418e9c9f60
SteveDaSteve <40248221+SteveDaSteve@users.noreply.github.com>
2018-06-23 19:48:58 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -212,7 +212,7 @@ minetest.register_craft({
 		}
 })
 
-minetest.register_node("home_blocks:arrow_stone"{
+minetest.register_node("home_blocks:arrow_stone",{
 		description = "Arrow Stone",
 		paramtype2 = "facedir",
 		light_source = 6,
