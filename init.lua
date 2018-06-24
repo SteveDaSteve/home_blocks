ed39903e9f10e4beea5848da9465670671fd3649
SteveDaSteve <40248221+SteveDaSteve@users.noreply.github.com>
2018-06-23 23:17:51 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -199,7 +199,7 @@ minetest.register_craft({
 minetest.register_node("home_blocks:rough_stone",{
 		description = "Rough Stone",
 		tiles = {"home_blocks_pathstone_other.png"},
-		group = {stone = 1, cracky = 1},
+		groups = {stone = 1, cracky = 1},
 		diggable = true
 	})
 
