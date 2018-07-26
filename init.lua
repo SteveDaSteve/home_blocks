bae53676cec0922691cec27556c87a7aeb6717c1
SteveDaSteve <40248221+SteveDaSteve@users.noreply.github.com>
2018-07-25 21:14:36 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -767,14 +767,14 @@ minetest.register_node("home_blocks:music_box",{
 -- Tiki Torch --
 minetest.register_node("home_blocks:tiki_torch_bottom",{
 		description = "Tiki Torch",
-		tiles = {"home_blocks_tiki_torch_bottom.png"},
+		tiles = {"home_blocks_tiki_torch_stick.png"},
 		inventory_image = "home_blocks_tiki_torch_item.png",
 		wield_image = "home_blocks_tiki_torch_item.png",
 		drawtype = "nodebox",
 		node_box = {
 	type = "fixed",
 	fixed = {
-		{-0.1875, -0.5000, -0.1875, 0.1875, 0.5000, 0.1875}
+		{-0.1250, -0.5000, -0.1250, 0.1250, 0.5000, 0.1250}
 	}
 },
 		paramtype = light,
@@ -802,8 +802,8 @@ minetest.register_node("home_blocks:tiki_torch_top", {
 		node_box = {
 	type = "fixed",
 	fixed = {
-		{-0.1875, -0.5000, -0.1875, 0.1875, 0.000, 0.1875},
-		{-0.2500, 0.000, -0.2500, 0.2500, 0.1875, 0.2500}
+		{-0.1250, -0.5000, -0.1250, 0.1250, -0.1875, 0.1250},
+		{-0.1250, -0.1875, -0.1250, 0.2500, 0.1875, 0.2500}
 	}
 },
 		paramtype = light,
